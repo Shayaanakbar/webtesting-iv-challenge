f4c39b466f5d2b9d394acc145605408cfc8a3bdd
shayaanakbar <shayaanakbar1106@gmail.com>
2019-06-26 22:29:01 -0500

    Backend testing working

--- api/server.js
+++ api/server.js
@@ -5,7 +5,7 @@ const server = express();
 server.use(express.json());
 
 server.get('/', async (req, res) => {
-  res.status(200).json({ message: welcome traveler! your server is up})
+  res.status(200).json({message: 'Welcome traveler! Your server is running'})
 });
 
 module.exports = server;
