21357191670da82a96bd4483b4ff8a19c928d9a2
shayaanakbar <shayaanakbar1106@gmail.com>
2019-06-27 11:35:14 -0500

    Incorporated GetAll, working

--- travelers/travelersModel.spec.js
+++ travelers/travelersModel.spec.js
@@ -11,7 +11,7 @@ describe('The Travelers Model', () => {
 
   describe('The insert function', () => {
     it('traveler is inserted', async () => {
-      await Travelers.insert({ name: 'Jamar'});
+      await Travelers.insert({name: 'Jamar'});
 
       const travelers = await db('travelers');
       expect(travelers.length).toBe(1);
@@ -21,7 +21,7 @@ describe('The Travelers Model', () => {
     it('should return the traveler by id', async () => {
 
 
-      const traveler = await Travelers.insert({ name: 'Ryan'})
+      const traveler = await Travelers.insert({name: 'Ryan'})
 
       expect(traveler.id).toBe(1);
       expect(traveler.name).toBe('Ryan');
@@ -38,13 +38,23 @@ describe('The Travelers Model', () => {
   });
 
   it('should add two travelers, delete one and only have one traveler listed', async () => {
-    await Travelers.insert({ name: 'Juan'});
-    const newTraveler1 = await Travelers.insert({ name: 'Emerson'});
+    await Travelers.insert({name: 'Juan'});
+    const newTraveler1 = await Travelers.insert({name: 'Emerson'});
 
 
     await Travelers.remove(newTraveler1.id)
     const traveler = await db('travelers');
 
     expect(traveler).toHaveLength(1);
+  });
+
+  describe('The delete function', () => {
+    it('should return list of all travelers', async () => {
+      await Travelers.getAll()
+
+      const travelers = db('travelers');
+
+      expect(travelers).toBeTruthy()
+    });
   });
 });
